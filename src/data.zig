66d46c6d3cf54393ea83ffefe41018c3a920b8cd
missrce <vcs@rce.run>
2024-11-04 03:09:00 +0000

    refactor: move to separate function

--- src/data.zig
+++ src/data.zig
@@ -1,18 +1,24 @@
 const std = @import("std");
 const fs = std.fs;
 
-pub fn get_appdata_dir(allocator: std.mem.Allocator) ![]const u8 {
-    const appdata = try fs.getAppDataDir(allocator, "charlotte");
-
-    if (fs.openDirAbsolute(appdata, .{})) |_| {
-        return appdata;
+fn create_if_not_found(path: []const u8) !void {
+    if (fs.openDirAbsolute(path, .{})) |_| {
+        return;
     } else |err| switch (err) {
         error.FileNotFound => {
-            try fs.makeDirAbsolute(appdata);
-            return appdata;
+            try fs.makeDirAbsolute(path);
+            return;
         },
         else => return err,
     }
+}
+
+pub fn get_appdata_dir(allocator: std.mem.Allocator) ![]const u8 {
+    const appdata = try fs.getAppDataDir(allocator, "charlotte");
+
+    try create_if_not_found(appdata);
+
+    return appdata;
 }
 
 test "get_appdata_dir memory leak test" {
