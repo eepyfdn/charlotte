2377a7dfa84871af79a6cad0b12bd7a31e9c3619
missrce <vcs@rce.run>
2024-11-04 00:03:40 +0000

    feat: basic CLI functionality

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, {}!\n", .{"World"});
+    const allocator = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Please provide the app to operate on.\nUsage: {s} <app> <operation>\n", .{args[0]});
+        return;
+    }
+    if (args.len < 3) {
+        std.debug.print("Please provide the operation to perform.\nUsage: {s} <app> <operation>\n", .{args[0]});
+        return;
+    }
+
+    // const app = args[1];
+    // const operation = args[2];
 }
